8860625f1beeab44e1dd2ed2ec784217adb4767c
Kazuki Mizuno <k.mizuno@appdevteam.work>
2021-04-28 21:16:25 +0900

    描画したものを画像に変換し、photoLibraryに保存するようにした

--- ARKitMission/NextViewController.swift
+++ ARKitMission/NextViewController.swift
@@ -37,7 +37,35 @@ class NextViewController: UIViewController {
                              width: self.imageView.frame.width,
                              height: self.imageView.frame.height)
         self.imageView.addSubview(drawView)
+        
+        //カメラロールに保存
+        save()
     }
     
-
+    // viewをimageに変換しカメラロールに保存する
+    private func save() {
+        // viewをimageとして取得
+        let image : UIImage = drawView.convertToImage()
+        
+        // カメラロールに保存する
+        UIImageWriteToSavedPhotosAlbum(image,self,#selector(self.didFinishSavingImage(_:didFinishSavingWithError:contextInfo:)),
+                                       nil)
+    }
+    
+    // 保存を試みた結果を受け取る
+    @objc func didFinishSavingImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+        
+        // 結果によって出すアラートを変更する
+        var title = "保存完了"
+        var message = "カメラロールに保存しました"
+        
+        if error != nil {
+            title = "エラー"
+            message = "保存に失敗しました"
+        }
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
