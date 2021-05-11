8e7f3b4e9974a495ed9ea17e17b00b5c70c49ded
Kazuki Mizuno <k.mizuno@appdevteam.work>
2021-05-11 21:16:33 +0900

    クラスを構造体に変更

---
+++ ARKitMission/ItemModel.swift
@@ -0,0 +1,100 @@
+//
+//  ItemModel.swift
+//  ARKitMission
+//
+//  Created by 水野一機 on 2021/05/11.
+//
+
+import ARKit
+
+/// 計算処理やデータの保存などを行う
+struct ItemModel {
+    
+    /// 選択したアイテムの名前
+    var selectedItem: String? = "plant"
+    /// アイテムを配置した座標の配列
+    var items: [simd_float4] = []
+    /// 2点間の距離の配列
+    var distances: [Float] = []
+    /// 2点間距離のしきい値
+    private let threshold: Float = 10.0
+    /// nodeを配置するView
+    private let sView: ARSCNView!
+    
+    init(view: ARSCNView) {
+        self.sView = view
+    }
+    
+    /// アイテムを配置する
+    mutating func addItem(hitTestResult: ARHitTestResult) {
+        if let selectedItem = self.selectedItem {
+            // scnファイルから3Dモデルのノードを作成
+            let scene = SCNScene(named: "\(selectedItem).scn",
+                                 inDirectory: "Models.scnassets")
+            let node = (scene?.rootNode.childNode(withName: selectedItem,
+                                                  recursively: false))!
+            
+            let transform = hitTestResult.worldTransform
+            let thirdColumn = transform.columns.3
+            
+            // 配列に追加
+            items.append(thirdColumn)
+            
+            // 3Dモデルを配置
+            node.position = SCNVector3(x: thirdColumn.x,
+                                       y: thirdColumn.y,
+                                       z: thirdColumn.z)
+            
+            node.scale = SCNVector3(0.005, 0.005, 0.005)
+            
+            node.name = selectedItem
+            
+            // シーンに追加
+            sView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    /// アイテムを削除する
+    mutating func removeItem(hitTestResult: SCNHitTestResult) {
+        let transform = hitTestResult.modelTransform
+        if hitTestResult.node.parent?.name == selectedItem {
+            for i in 0 ..< items.count {
+                // itemsの座標と等しいかどうか
+                if (items[i].x == transform.m41) && (items[i].y == transform.m42) && (items[i].z == transform.m43) {
+                    items.remove(at: i)
+                    hitTestResult.node.parent?.removeFromParentNode()
+                    break
+                }
+            }
+        }
+    }
+    
+    /// アイテム間の距離がしきい値未満かどうか
+    func checkItemsDistance() -> Bool {
+        // 最初のアイテム
+        let sItem = items[0]
+        // 最後のアイテム
+        let eItem = items[items.count - 1]
+        
+        let distance = calcDistance(start: sItem, end: eItem)
+        return distance < threshold
+    }
+    
+    // 距離の値を保存(ラベル出力用)
+    mutating func saveDistance() {
+        for i in 0 ..< items.count - 1 {
+            let d = calcDistance(start: items[i], end: items[i + 1])
+            distances.append(d)
+        }
+    }
+    
+    /// 2点間距離の計算
+    func calcDistance(start: simd_float4, end: simd_float4) -> Float {
+        let d: Float
+        let pos = SCNVector3Make(end.x - start.x,
+                                 end.y - start.y,
+                                 end.z - start.z)
+        d = sqrt(pos.x * pos.x + pos.y * pos.y + pos.z * pos.z )
+        return d
+    }
+}
